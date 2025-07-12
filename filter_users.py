6e0e82173230593956c15a579188a5121aa7b17e
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-07-12 14:46:46 +0200

    Added feature 'fiter by email' and updated user input validation

--- filter_users.py
+++ filter_users.py
@@ -12,6 +12,7 @@ def filter_users_by_name(name):
     if not filtered_users:
         print(f"No user found with name '{name}'.")
 
+
 def filter_users_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
@@ -24,8 +25,20 @@ def filter_users_by_age(age):
         print(f"No user found with age '{age}'.")
 
 
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+    for user in filtered_users:
+        print(user)
+
+    if not filtered_users:
+        print(f"No user found with email '{email}'.")
+
+
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? Choose from <Name> / <Age>: ").strip().lower()
+    filter_option = input("What would you like to filter by? Choose from <Name> / <Age> / <Email>: ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
@@ -33,5 +46,8 @@ if __name__ == "__main__":
     elif filter_option == "age":
         age_to_search = int(input("Enter age to filter user: "))
         filter_users_by_age(age_to_search)
+    elif filter_option == "email":
+        email_to_search = input("Enter email to filter user: ").strip()
+        filter_users_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.") #Change validation
